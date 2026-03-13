292c44166481a030a300fd92e575273f924f3c48
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-13 15:46:45 +0100

    update

---
+++ src/app/about/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  // Define the Loading UI here
+  //return <div>Loading...</div>
+
+
+  
+}
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,28 @@
-export default function AboutPage(){
-    return(
-        <>
-            <h1>About Page!!</h1>
-        </>
+export const dynamic = 'force-dynamic';
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+export default async function AboutPage(){  //! async
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts/1', { next: {revalidate: 3600} });
+    const repos: Post[] = await response.json();
+
+    return (
+      <>
+        <h1>About Page!!</h1>
+
+        <ul>
+          {repos.map((repo) => (
+            <li key={repo.id}>
+              <div>{repo.title}</div>
+            </li>
+          ))}
+        </ul>
+      </>
     );
 
 }
