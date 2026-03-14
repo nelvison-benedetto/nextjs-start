19e943084a5ca6cecc808dd9285fae482e33a306
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-03-14 10:26:33 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";  //FONTS!
 import "./globals.css";
 import Header from "@/components/header";
 
